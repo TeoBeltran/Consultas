37959d83feba068fb195857bb2aae8bfc4b534c2
Teo Beltrán <109324334+TeoBeltran@users.noreply.github.com>
2025-07-10 17:56:48 -0300

    Update Mover PV + num.sql

--- Mover PV + num.sql
+++ Mover PV + num.sql
@@ -1,14 +1,14 @@
 --BKP
-SELECT * INTO Comprobantes_07032025 FROM Comprobantes
-SELECT * INTO ComprobantesDetalle_07032025 FROM ComprobantesDetalle
-SELECT * INTO ComprobantesmImpuestos_07032025 FROM ComprobantesImpuestos
-SELECT * INTO ComprobantesMediosPagos_07032025 FROM ComprobantesMediosPagos
-SELECT * INTO Caja_07032025 FROM Caja
-SELECT * INTO CajaMovimientos_07032025 from CajaMovimientos
-SELECT * INTO C_Deudas_07032025 FROM C_Deudas
-SELECT * INTO C_Pagos_07032025 FROM C_Pagos
-SELECT * INTO Asientos_07032025 From Asientos
-SELECT * INTO AsientosDetalle_07032025 From AsientosDetalle
+SELECT * INTO Comprobantes_10072025 FROM Comprobantes
+SELECT * INTO ComprobantesDetalle_10072025 FROM ComprobantesDetalle
+SELECT * INTO ComprobantesmImpuestos_10072025 FROM ComprobantesImpuestos
+SELECT * INTO ComprobantesMediosPagos_10072025 FROM ComprobantesMediosPagos
+SELECT * INTO Caja_10072025 FROM Caja
+SELECT * INTO CajaMovimientos_10072025 from CajaMovimientos
+SELECT * INTO C_Deudas_10072025 FROM C_Deudas
+SELECT * INTO C_Pagos_10072025 FROM C_Pagos
+SELECT * INTO Asientos_10072025 From Asientos
+SELECT * INTO AsientosDetalle_10072025 From AsientosDetalle
 
 --VARIABLES----------------------------------------------------------------------------------------
 declare @pv int set @pv=9 --Define Punto de Venta
@@ -50,28 +50,28 @@ where Pv = @pv and TipoCbte = @tipo and NroCbte Between @PrimerComp and @UltimoC
 --MOVER NUMERACION---------------------------------------------------------------------------------
 
 update Comprobantes set Numero = Numero - ( ( @UltimoComp - @PrimerComp ) + 1 )
-where PuntoVenta = @pv and Tipo= @tipo and Numero >= @UltimoComp +1
+where PuntoVenta = @pv and Tipo= @tipo and Numero >= @UltimoComp + 1
 
 update ComprobantesDetalle set Numero = Numero - ( ( @UltimoComp - @PrimerComp ) + 1 )
-where PuntoVenta = @pv and Tipo= @tipo and Numero = @UltimoComp +1
+where PuntoVenta = @pv and Tipo= @tipo and Numero = @UltimoComp + 1
 
 update ComprobantesImpuestos set Numero = Numero - ( ( @UltimoComp - @PrimerComp ) + 1 )
-where PuntoVenta = @pv and Tipo= @tipo and Numero = @UltimoComp +1
+where PuntoVenta = @pv and Tipo= @tipo and Numero = @UltimoComp + 1
 
 update ComprobantesMediosPagos set NroCbte = NroCbte - ( ( @UltimoComp - @PrimerComp ) + 1 )
-where PV = @pv and TipoCbte= @tipo and NroCbte = @UltimoComp +1
+where PV = @pv and TipoCbte= @tipo and NroCbte = @UltimoComp + 1
 
 update C_Deudas set NroCbte = NroCbte - ( ( @UltimoComp - @PrimerComp ) + 1 )
-where Pv = @pv and TipoCbte = @tipo and NroCbte = @UltimoComp +1
+where Pv = @pv and TipoCbte = @tipo and NroCbte = @UltimoComp + 1
 
 update C_Pagos set NroCbte = NroCbte - ( ( @UltimoComp - @PrimerComp ) + 1 )
-where Pv = @pv and TipoCbte = @tipo and NroCbte = @UltimoComp +1
+where Pv = @pv and TipoCbte = @tipo and NroCbte = @UltimoComp + 1
 
 update Caja set NroCbte = NroCbte - ( ( @UltimoComp - @PrimerComp ) + 1 )
-where Pv = @pv and TipoCbte = @tipo and NroCbte = @UltimoComp +1
+where Pv = @pv and TipoCbte = @tipo and NroCbte = @UltimoComp + 1
 
 update Asientos set NroCbte = NroCbte - ( ( @UltimoComp - @PrimerComp ) + 1 )
-where Pv = @pv and TipoCbte = @tipo and NroCbte = @UltimoComp +1
+where Pv = @pv and TipoCbte = @tipo and NroCbte = @UltimoComp + 1
 
 
 
